99d0abd284a9699eb3c14d9df26bfe90db3b4ae2
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-10 18:31:23 +0530

    add counter cache for followers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910122240) do
+ActiveRecord::Schema.define(version: 20170910125428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,13 +60,15 @@ ActiveRecord::Schema.define(version: 20170910122240) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "email",                                        null: false
+    t.string   "encrypted_password",   limit: 128,             null: false
+    t.string   "confirmation_token",   limit: 128
+    t.string   "remember_token",       limit: 128,             null: false
     t.string   "username"
+    t.integer  "followed_users_count",             default: 0, null: false
+    t.integer  "followers_count",                  default: 0, null: false
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
